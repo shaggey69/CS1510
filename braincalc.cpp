c8e3f8d645be51b8327be18c92d4e24f2d1bc746
Ari <azsxt5@mst.edu>
2017-03-05 23:14:26 -0600

    actully crwated the file.. not much else LOL FML

---
+++ braincalc.cpp
@@ -0,0 +1,17 @@
+/*
+Programmer: Ari Sherman 
+Section: 1510 A
+Date: 3/15/2017
+Description: brain lab implement 
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main ()
+{
+
+
+	return 0;
+}
